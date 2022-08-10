ad0c441642913e2f8f74986058ee09390906284a
Mawludin <chester3230@yandex.ru>
2022-08-10 21:32:40 +0300

    Третье ДЗ

--- src/Main.java
+++ src/Main.java
@@ -19,7 +19,7 @@ public class Main {
         int firstFriday = 6;
         int lastDay = 31;
 
-        for (int i = firstFriday; i <= 31; i = i + 7 ){
+        for (int i = firstFriday; i <= lastDay; i = i + 7 ){
             System.out.println("Сегодня пятница, "+ i +"-е число. Необходимо подготовить отчет");
         }
 
